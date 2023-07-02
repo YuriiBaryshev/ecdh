c7221908f78ec3ea17d25fbf5d80c7da6b7c762f
YuriiBaryshev <yura.baryshev@gmail.com>
2023-07-02 10:20:44 +0300

    create project's carcass

---
+++ example/ecdh_example.dart
@@ -0,0 +1,6 @@
+import 'package:ecdh/ecdh.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/ecdh.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library ecdh;
+
+export 'src/ecdh_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
