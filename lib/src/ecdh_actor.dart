a7655f6b8f730e45ce457e0f10eecc547d2c24e9
YuriiBaryshev <yura.baryshev@gmail.com>
2023-07-04 20:57:21 +0300

    add secret generation

--- lib/src/ecdh_actor.dart
+++ lib/src/ecdh_actor.dart
@@ -1,4 +1,18 @@
-///Represents actor/party of ECDH protocol
-class ECDHActor {
+import 'dart:math';
 
-} 
+///Represent actor/party of ECDH protocol
+class ECDHActor {
+  late BigInt _secret;
+
+  ///Generate secret of the party
+  void _generateSecret({int bitLength = 256}) {
+    if(bitLength & 7 != 0) {
+      throw ArgumentError("Secret value's length should multiple 8");
+    }
+    Random generator = Random.secure();
+    _secret = BigInt.zero;
+    for(int i = 0; i < (bitLength >> 3); i++) {
+      _secret = (_secret << 8) + BigInt.from(generator.nextInt(256));
+    }
+  }
+}
