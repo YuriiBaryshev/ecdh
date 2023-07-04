fef3bbb59dc00799df0332784dfdb0eb8a8e9a3d
YuriiBaryshev <yura.baryshev@gmail.com>
2023-07-04 21:16:02 +0300

    add constructor

--- lib/src/ecdh_actor.dart
+++ lib/src/ecdh_actor.dart
@@ -1,8 +1,18 @@
 import 'dart:math';
+import 'package:elliptic_curves_facade/elliptic_curves_facade.dart';
+import 'package:elliptic/elliptic.dart';
 
 ///Represent actor/party of ECDH protocol
 class ECDHActor {
   late BigInt _secret;
+  late EllipticCurveFacade _facade;
+
+
+  ECDHActor([EllipticCurve? ec]) {
+    ec ??= getSecp256k1() as EllipticCurve;
+    _facade = EllipticCurveFacade(ec);
+    generateSecret(ec.n);
+  }
 
   ///Generate secret of the party
   void generateSecret(BigInt maxValue) {
