4a0a36529927cd9e01e7cd44ee7648d93becac12
YuriiBaryshev <yura.baryshev@gmail.com>
2023-07-04 21:37:59 +0300

    add member for used elliptic curve

--- lib/src/ecdh_base.dart
+++ lib/src/ecdh_base.dart
@@ -1,13 +1,16 @@
 import 'package:elliptic/elliptic.dart';
+import 'package:elliptic_curves_facade/elliptic_curves_facade.dart';
 
 import './ecdh_actor.dart';
 
 /// Checks if you are awesome. Spoiler: you are.
 class ECDH {
   late ECDHActor alice, bob;
+  late EllipticCurve _ec;
 
   ///Simulate elliptic curve Diffie-Hellman protocol for two parties
   ECDH([EllipticCurve? ec]) {
+    _ec = ec ?? getSecp256k1() as EllipticCurve;
     alice = ECDHActor(ec);
     bob = ECDHActor(ec);
   }
