0452a119c2b8139fe27fcbfef8f7103833b7b7eb
YuriiBaryshev <yura.baryshev@gmail.com>
2023-07-05 14:59:45 +0300

    add example for 3 parties

--- example/ecdh_example.dart
+++ example/ecdh_example.dart
@@ -1,6 +1,27 @@
-import 'package:ecdh/ecdh.dart';
+import 'package:ecdh/src/ecdh_actor.dart';
+import 'package:elliptic_curves_facade/elliptic_curves_facade.dart';
+import 'package:elliptic/elliptic.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  ECDHActor alice, bob, carl;
+  EllipticCurve ec = getP256();
+  //initialization and computing personal part of secret by each party
+  alice = ECDHActor(ec);
+  bob = ECDHActor(ec);
+  carl = ECDHActor(ec);
+  //generating G*secret points
+  ECPoint fromAlice1 = alice.yieldPoint();
+  ECPoint fromBob1 = bob.yieldPoint();
+  ECPoint fromCarl1 = carl.yieldPoint();
+  //Points' exchange round 1
+  ECPoint fromAlice2 = alice.yieldPoint(fromCarl1);
+  ECPoint fromBob2 = bob.yieldPoint(fromAlice1);
+  ECPoint fromCarl2 = carl.yieldPoint(fromBob1);
+  //Points' exchange round2
+  alice.yieldPoint(fromCarl2);
+  bob.yieldPoint(fromAlice2);
+  carl.yieldPoint(fromBob2);
+  //comparison of results
+  print(alice.getMutualSecret() == bob.getMutualSecret());//true
+  print(bob.getMutualSecret() == carl.getMutualSecret());//true
 }
